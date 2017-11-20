cb543c72674faea87cae547d11a94d7cbc605152
dovschuman <schuman.dov@gmail.com>
2017-11-20 23:50:03 +0200

    commit put

--- UsersManager/Controllers/UsersController.cs
+++ UsersManager/Controllers/UsersController.cs
@@ -37,5 +37,10 @@ namespace UsersManager.Controllers
             //    }
             //};
         }
+
+        public string Put([FromBody]string value)
+        {
+            return "Put returning: " + value;
+        }
     }
 }
